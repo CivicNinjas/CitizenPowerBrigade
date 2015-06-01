ca698b58b0a678a60ac4bd8ffe67f653b5124034
harbieism <jow598@utulsa.edu>
2015-06-01 15:15:47 -0500

    Add owner to powercar model

--- powermap/models.py
+++ powermap/models.py
@@ -1,13 +1,16 @@
 from django.contrib.gis.db import models
+from django.contrib.auth.models import User
+
 
 class PowerCar(models.Model):
     # A car outfitted to provied power.
     name = models.CharField(max_length=255)
     vehicle_description = models.CharField(max_length=255)
-    license_plate = models.CharField(max_length=15) 
+    license_plate = models.CharField(max_length=15)
     eta = models.DateTimeField()
     current_location_until = models.DateTimeField()
     current_location = models.PointField(help_text="Current location")
     target_location = models.PointField()
     next_location = models.PointField()
+    owner = models.ForeignKey(User, null=True)
     objects = models.GeoManager()
