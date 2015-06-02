813c5bf7969308dc5e67ab6cbe3c84eaf1e21e80
harbieism <jow598@utulsa.edu>
2015-06-02 14:52:44 -0500

    Add index map page and view to it

---
+++ powermap/static/powermap/index.js
@@ -0,0 +1,4 @@
+// Provide your access token
+L.mapbox.accessToken = 'pk.eyJ1IjoiaGFyYmllaXNtIiwiYSI6IksyU1Rkc0UifQ.eXciAIxM0pfdj5STBHNnbQ';
+// Create a map in the div #map
+L.mapbox.map('map', 'harbieism.mbb67n8i');
