210ebfaefac2122c6dab2417ab1cd9bad5ef817e
harbieism <jow598@utulsa.edu>
2015-06-02 15:45:26 -0500

    Add ViewSets for cars and users

--- powermap/views.py
+++ powermap/views.py
@@ -1,5 +1,25 @@
+from powermap.models import PowerCar
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from pttp.powermap.serializers import PowerCarSerializer, UserSerializer
 from django.shortcuts import render
 
-# Create your views here.
+
+class PowerCarViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows cars to be viewed or edited.
+    """
+    queryset = PowerCar.objects.all()
+    serializer_class = PowerCarSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
 def index(request):
     return render(request, 'powermap/index.html', {})
