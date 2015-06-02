ee98ec27fca24013caf31bef6e9d4643814f76d3
harbieism <jow598@utulsa.edu>
2015-06-02 14:52:20 -0500

    Fixed bug with URL inclusion

--- pttp/urls.py
+++ pttp/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^pttp/', include('pttp.urls')),
+    url(r'^pttp/', include('powermap.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
