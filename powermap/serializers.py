714addc1ee42d3403ce63cc5306bc7c9d41a5b5d
harbieism <jow598@utulsa.edu>
2015-06-04 11:15:15 -0500

    Add serializer for HelpNote model

--- powermap/serializers.py
+++ powermap/serializers.py
@@ -1,4 +1,4 @@
-from powermap.models import PowerCar
+from powermap.models import PowerCar, HelpNote
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
@@ -8,6 +8,12 @@ class PowerCarSerializer(GeoFeatureModelSerializer):
     class Meta:
         model = PowerCar
         geo_field = 'current_location'
+
+
+class HelpNoteSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = HelpNote
+        geo_field = 'location'
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
