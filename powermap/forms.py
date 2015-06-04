dca6f3045a1a9ab8532f891ff5125dffe78ae599
harbieism <jow598@utulsa.edu>
2015-06-04 13:48:28 -0500

    Add crispy_form for HelpNote model

---
+++ powermap/forms.py
@@ -0,0 +1,37 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
+from django import forms
+from powermap.models import HelpNote
+
+
+class HelpNoteModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(HelpNoteModelForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.layout.append(Submit('save', 'save'))
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'create_note/'
+        self.helper.layout = Layout(
+            Fieldset(
+                'Post a note about your situation',
+                'address',
+                'message',
+                'creator',
+                'location'
+            ),
+            ButtonHolder(
+                Submit('submit', 'Submit', css_class='button white')
+            )
+        )
+
+    class Meta:
+        model = HelpNote
+        fields = [
+            'address',
+            'message',
+            'creator',
+            'location'
+        ]
+        widgets = {
+            'location': forms.HiddenInput()
+        }
