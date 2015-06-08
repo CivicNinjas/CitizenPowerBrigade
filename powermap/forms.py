b432c977f6cd4ac2a599da7fe5ac64a1fb94ff50
harbieism <jow598@utulsa.edu>
2015-06-08 11:27:43 -0500

    Fix flake8 errors and url target

--- powermap/forms.py
+++ powermap/forms.py
@@ -1,5 +1,5 @@
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
+from crispy_forms.layout import Layout, ButtonHolder, Submit
 from django import forms
 from powermap.models import HelpNote
 
@@ -10,13 +10,12 @@ class HelpNoteModelForm(forms.ModelForm):
         self.helper = FormHelper(self)
         self.helper.layout.append(Submit('save', 'save'))
         self.helper.form_method = 'post'
-        self.helper.form_action = 'create_note/'
+        self.helper.form_action = ''
         self.helper.layout = Layout(
-                'address',
-                'message',
-                'creator',
-                'location',
-            
+            'address',
+            'message',
+            'creator',
+            'location',
             ButtonHolder(
                 Submit('submit', 'Submit', css_class='button white')
             )
