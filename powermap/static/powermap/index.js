07bfd117ebfc1175f0f34759c9e6bff70a1d9a48
harbieism <jow598@utulsa.edu>
2015-06-08 10:01:49 -0500

    Remove log statements, add icons to each point

--- powermap/static/powermap/index.js
+++ powermap/static/powermap/index.js
@@ -10,8 +10,11 @@ var noteLayer = L.mapbox.featureLayer().addTo(map);
 
 var getData = (function() {
     $.get("http://127.0.0.1:8000/powercars/?format=json", function(data) {
-        var dataStorage = data;
-        console.log(data.results);
+        for(var i = 0; i < data.results.features.length; i++){
+            data.results.features[i].properties["marker-symbol"] = "car";
+            data.results.features[i].properties["marker-size"] = "large";
+            data.results.features[i].properties["marker-color"] = "#fc4353";
+        }
         map.setView([50.11, 44.99], 10);
         myLayer.setGeoJSON(data.results);
     });
@@ -20,7 +23,11 @@ var getData = (function() {
 var getNotes = (function() {
     $.get("http://127.0.0.1:8000/helpnotes/?format=json", function(data) {
         var dataStorage = data;
-        console.log(data.results);
+        for(var i = 0; i < data.results.features.length; i++){
+            data.results.features[i].properties["marker-symbol"] = "oil-well";
+            data.results.features[i].properties["marker-size"] = "large";
+            data.results.features[i].properties["marker-color"] = "#fc4353";
+        }
         map.setView([50.11, 44.99], 10);
         noteLayer.setGeoJSON(data.results);
     });
@@ -28,7 +35,6 @@ var getNotes = (function() {
 
 myLayer.on('click', function(e){
     $.get("http://127.0.0.1:8000/pttp/popup/" + e.layer.feature.id + "/", function(data) {
-        console.log(data);
         e.layer.bindPopup(data);
         e.layer.openPopup();
     });
