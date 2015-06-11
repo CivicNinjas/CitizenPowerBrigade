f9838cb64f22952b152f1f7c8c8513f861add9b5
harbieism <jow598@utulsa.edu>
2015-06-11 12:52:28 -0500

    Add method to get location of other cars

--- powermap/static/powermap/index.js
+++ powermap/static/powermap/index.js
@@ -67,10 +67,43 @@ $( document ).ready(function() {
         });
         marksTheSpot.addTo(otherCars);
         newlatlngs = [start_lat_lng, end_lat_lng]
-        temp = L.polyline(newlatlngs, {color: '#abf696', opacity: "0.9"}).addTo(otherCars);
+        temp = L.polyline(newlatlngs, {color: '#abf696', opacity: "0.8"}).addTo(otherCars);
       }
     });
   });
+
+(function updateOtherCars() {
+  otherCars.setGeoJSON([]);
+  $.get("/pttp/cars/get_other_cars/", function(data) {
+    for(var i = 0; i < data.features.length; i++){
+      feat = data.features[i];
+      feat.properties["marker-symbol"] = "car";
+      feat.properties["marker-size"] = "large";
+      feat.properties["marker-color"] = "#bbf696";
+    }
+    otherCars.setGeoJSON([]);
+    otherCars.setGeoJSON(data)._layers;
+    for(var i = 0; i < data.features.length; i++){
+      feat = data.features[i];
+      start_lat_lng = new L.LatLng(feat.geometry.coordinates[1], feat.geometry.coordinates[0]);
+      end_lat_lng = new L.LatLng(
+          feat.properties.next_location.coordinates[1],
+          feat.properties.next_location.coordinates[0]
+      );
+      var marksTheSpot = L.marker(end_lat_lng, {
+        icon: L.mapbox.marker.icon({
+          'marker-symbol': 'cross',
+          'marker-size': 'medium',
+          'marker-color': '#bbf696'
+        }),
+      });
+      marksTheSpot.addTo(otherCars);
+      newlatlngs = [start_lat_lng, end_lat_lng]
+      temp = L.polyline(newlatlngs, {color: '#abf696', opacity: "0.8"}).addTo(otherCars);
+    }
+  });
+  setTimeout(updateOtherCars, 10000);
+})();
 
 
   (function workerTwo() {
