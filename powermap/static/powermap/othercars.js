a4536abb6589c287a7a22ec7015e165b014f3936
harbieism <jow598@utulsa.edu>
2015-06-25 10:54:35 -0500

    Remove deprecated block of code for init ajax call

--- powermap/static/powermap/othercars.js
+++ powermap/static/powermap/othercars.js
@@ -16,36 +16,6 @@ otherLayers.otherCars.on('click', function(e){
   }
 });
 
-var getOtherCars = (function() {
-  $.get("/powercars/other_active_cars/", function(data) {
-    for(var i = 0; i < data.car_data.features.length; i++){
-      var feat = data.car_data.features[i];
-      feat.properties["marker-symbol"] = "car";
-      feat.properties["marker-size"] = "large";
-      feat.properties["marker-color"] = "#FF4500";
-    };
-    otherLayers.otherCars.setGeoJSON(data.car_data)._layers;
-    for(var i = 0; i < data.car_data.features.length; i++){
-      var feat = data.car_data.features[i];
-      var startLatLng = new L.LatLng(feat.geometry.coordinates[1], feat.geometry.coordinates[0]);
-      var endLatLng = new L.LatLng(
-        feat.properties.next_location.coordinates[1],
-        feat.properties.next_location.coordinates[0]
-      );
-      var marksTheSpot = L.marker(endLatLng, {
-        icon: L.mapbox.marker.icon({
-          'marker-symbol': 'cross',
-          'marker-size': 'medium',
-          'marker-color': '#FF4500'
-        }),
-      });
-      marksTheSpot.addTo(otherLayers.otherCarMarkers);
-      var newlatlngs = [startLatLng, endLatLng]
-      var temp = L.polyline(newlatlngs, {color: '#FF4500', opacity: "0.8"}).addTo(otherLayers.otherCarMarkers);
-    }
-  });
-});
-
 (function updateOtherCars() {
   var tempPopup = null;
   var markerToPopup = null;
