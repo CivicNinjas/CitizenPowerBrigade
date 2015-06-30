e8f6291bde2d210e762c89a6d8a20dea2f6122ce
harbieism <jow598@utulsa.edu>
2015-06-30 10:28:28 -0500

    Remove deprecated url for the create_note view

--- powermap/urls.py
+++ powermap/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^popup/(?P<car_id>\d+)/$', views.popup, name='popup'),
     url(r'^note_form/$', views.note_form, name='note_form'),
-    url(r'^create_note/$', views.create_note, name='create_note'),
     url(
         r'^note_popup/(?P<note_id>\d+)/$',
         views.note_popup,
