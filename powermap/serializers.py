06d33c9307c424a674112d0389be790f0f505e0d
harbieism <jow598@utulsa.edu>
2015-06-30 14:22:27 -0500

    Add phone_number validate method to serializer

--- powermap/serializers.py
+++ powermap/serializers.py
@@ -1,5 +1,6 @@
 from powermap.models import PowerCar, HelpNote, Diagnostic, Inverter, GPS
 from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
@@ -21,6 +22,26 @@ class HelpNoteSerializer(GeoFeatureModelSerializer):
     class Meta:
         model = HelpNote
         geo_field = 'location'
+
+    def validate_phone_number(self, value):
+        """
+        Ensure that the phone number is valid.
+        If it is still a working number, strip useful info from it.
+        """
+        cleaned_num = value
+        if cleaned_num != "":
+            stripped_num = ''.join(x for x in cleaned_num if x.isdigit())
+            if len(stripped_num) == 10:
+                return "+1" + stripped_num
+            elif len(stripped_num) == 11 and stripped_num[0] == "1":
+                return "+" + stripped_num
+            else:
+                raise serializers.ValidationError(
+                    _("Invalid phone number: %(number)s."),
+                    code="invalid",
+                    params={"number": cleaned_num},
+                )
+        return cleaned_num
 
 
 class DiagnosticSerializer(serializers.HyperlinkedModelSerializer):
