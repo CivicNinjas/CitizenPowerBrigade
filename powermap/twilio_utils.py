e53256bb57f4bf2509de916740253d7445ccbf3f
harbieism <jow598@utulsa.edu>
2015-06-30 10:29:42 -0500

    Add try except block to send_alerts

--- powermap/twilio_utils.py
+++ powermap/twilio_utils.py
@@ -1,3 +1,4 @@
+import twilio
 from twilio.rest import TwilioRestClient
 from pttp.settings import TWILIO_AUTH
 
@@ -9,30 +10,33 @@ def send_alerts(numbers, car, event_type):
     """
     account_sid = TWILIO_AUTH["SID"]
     auth_token = TWILIO_AUTH["AUTH_TOKEN"]
-    client = TwilioRestClient(account_sid, auth_token)
-    message_body = ""
-    if event_type == "Arrival":
-        message_body = (
-            "A PowerCar (Plate #{plate_number}) has arrived in your area!"
-            + "It will be there until {leave_time}."
-        ).format(
-            leave_time=car.current_location_until,
-            plate_number=car.license_plate
-        )
-    elif event_type == "SetDestination":
-        message_body = (
-            "A PowerCar (Plate #{plate_number}) will be in your area!"
-            + " It expects to arrive at {arrive_time} and to be"
-            + " there until {leave_time}."
-        ).format(
-            arrive_time=car.eta,
-            leave_time=car.current_location_until,
-            plate_number=car.license_plate
-        )
-    for number in numbers:
-        message = client.messages.create(
-            body=message_body,
-            to=number,
-            from_=TWILIO_AUTH["TWILIO_NUMBER"]
-        )
-        print message.sid
+    try:
+        client = TwilioRestClient(account_sid, auth_token)
+        message_body = ""
+        if event_type == "Arrival":
+            message_body = (
+                "A PowerCar (Plate #{plate_number}) has arrived in your area!"
+                + "It will be there until {leave_time}."
+            ).format(
+                leave_time=car.current_location_until,
+                plate_number=car.license_plate
+            )
+        elif event_type == "SetDestination":
+            message_body = (
+                "A PowerCar (Plate #{plate_number}) will be in your area!"
+                + " It expects to arrive at {arrive_time} and to be"
+                + " there until {leave_time}."
+            ).format(
+                arrive_time=car.eta,
+                leave_time=car.current_location_until,
+                plate_number=car.license_plate
+            )
+        for number in numbers:
+            message = client.messages.create(
+                body=message_body,
+                to=number,
+                from_=TWILIO_AUTH["TWILIO_NUMBER"]
+            )
+            print message.sid
+    except twilio.TwilioRestException as e:
+        print e
