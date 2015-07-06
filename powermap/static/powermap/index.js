3525cab1d723144440356043386b9f59e6ddce5e
harbieism <jow598@utulsa.edu>
2015-07-06 12:25:47 -0500

    Prefer Canvas to improve Firefox rendering

--- powermap/static/powermap/index.js
+++ powermap/static/powermap/index.js
@@ -1,5 +1,7 @@
 'use strict'
+var L_PREFER_CANVAS = true;
 var map = require('./map');
 var otherCars = require('./othercars');
 var ownerCar = require('./ownercar');
 var notes = require('./helpnotes');
+var admin = require('./adminView')
