f82eacaa396dbc1bf82fe8469f618d48abbada25
harbieism <jow598@utulsa.edu>
2015-07-06 14:48:33 -0500

    Create tests for HelpNote viewset

--- powermap/tests.py
+++ powermap/tests.py
@@ -1,10 +1,15 @@
-import copy
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import AnonymousUser, User
 from models import HelpNote
+
 from serializers import HelpNoteSerializer
+from rest_framework.test import APIRequestFactory, APITestCase, force_authenticate
+
 from forms import HelpNoteModelForm
 from django.contrib.gis.geos import Point, GEOSGeometry
 
+from .views import HelpNoteViewSet
 
 class HelpNoteTest(TestCase):
     """
@@ -70,3 +75,43 @@ class HelpNoteTest(TestCase):
         self.assertTrue(form.is_valid())
         help_note = form.save()
         self.assertEqual(help_note.phone_number, "+12234567891")
+
+
+class HelpNoteViewSetTest(APITestCase):
+    """
+    Test the results and authentication of
+    the HelpNoteViewSet
+    """
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='jacob', email='jacob@cool.com', password="x"
+        )
+        HelpNote.objects.create(
+            address="S Test Ave",
+            message="Help me",
+            creator="Test A. Test",
+            location=Point(45.50, 55.6)
+        )
+
+    def test_list(self):
+        """
+        Test the list API endpoint.
+        """
+        url = '/helpnotes/?format=json'
+
+        # Request is not authenticated, so it should return a 403
+        # unauthorized status code.
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 403)
+
+        # Test for whether a valid, logged-in user can access the list.
+        factory = APIRequestFactory()
+        user = self.user
+        view = HelpNoteViewSet.as_view({'get': 'list'})
+
+        request = factory.get(url)
+        force_authenticate(request, user=user)
+        response = view(request)
+
+        self.assertEqual(response.status_code, 200)
