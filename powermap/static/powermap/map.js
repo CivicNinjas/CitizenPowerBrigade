0dec2d9eaa7f376ebec2b5b3839baa2b97f1de95
harbieism <jow598@utulsa.edu>
2015-07-07 11:09:15 -0500

    Remove deprecated mapInfo attribute

--- powermap/static/powermap/map.js
+++ powermap/static/powermap/map.js
@@ -2,40 +2,11 @@
 
 L.mapbox.accessToken = 'pk.eyJ1IjoiaGFyYmllaXNtIiwiYSI6IksyU1Rkc0UifQ.eXciAIxM0pfdj5STBHNnbQ';
 
-
 var Map = function () {
   this.map = L.mapbox.map('map', 'harbieism.mbb67n8i');
-  this.mapInfo = {
-    currentPopup: {
-      type: null,
-      id: null,
-      popup: null
-    }
-  };
-
   this.map.addControl(L.mapbox.geocoderControl('mapbox.places', {
     autocomplete: true
   }));
-
-  this.map.on('popupopen', function(e) {
-    if (e.popup._source.feature != null) {
-      if (e.popup._source.feature.properties["marker-symbol"] == "car") {
-        this.mapInfo.currentPopup.type = "PowerCar";
-      } else if (e.popup._source.feature.properties["marker-symbol"] == "oil-well") {
-        this.mapInfo.currentPopup.type = "HelpNote";
-      }
-      this.mapInfo.currentPopup.id = e.popup._source.feature.id;
-      this.mapInfo.currentPopup.popup = e.popup;
-    };
-  });
-
-  this.map.on('popupclose', function(e) {
-    this.mapInfo.currentPopup.type = null;
-    this.mapInfo.currentPopup.id = null;
-    this.mapInfo.currentPopup.popup = null;
-  });
-
-
 };
 
 Map.prototype.setLocation = function(position) {
