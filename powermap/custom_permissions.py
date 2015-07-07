d48a1b15904e14af0f45766c89993a0b36ed6b2d
harbieism <jow598@utulsa.edu>
2015-07-07 11:00:17 -0500

    Create a WriteOnlyOrUser permission

--- powermap/custom_permissions.py
+++ powermap/custom_permissions.py
@@ -5,3 +5,12 @@ class IsAdminOrCarOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.user == obj.owner or request.user.is_staff
+
+
+class WriteOnlyOrUser(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method == "POST":
+            return True
+        else:
+            return request.user.is_authenticated()
