b6f7b68523f211ec2165cbf44dd8a6f467d5a8d1
harbieism <jow598@utulsa.edu>
2015-07-07 11:23:49 -0500

    Create lint task in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 var browserify = require('browserify');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default', ['lint']);
+
+gulp.task('lint', function() {
+    return gulp.src('./powermap/static/powermap/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'));
 });
